6cccbd83354ce845ff77db0f97e666aecb1aa57e
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-04 00:46:10 +0700

    Update HomeViewModel.swift

--- HomeViewModel.swift
+++ HomeViewModel.swift
@@ -7,32 +7,10 @@ class HomeViewModel: ObservableObject {
     @Published var nowPlaying: [Movie] = []
     @Published var topRated: [Movie] = []
     @Published var genres: [Genre] = []
-    @Published var isLoading = true
+    @Published var isLoading = false
     
     func loadAll() async {
-        isLoading = true
-        
-        do {
-            let t = try await APIService.shared.trending()
-            let u = try await APIService.shared.upcoming()
-            let n = try await APIService.shared.nowPlaying()
-            let tr = try await APIService.shared.topRated()
-            let g = try await APIService.shared.genres()
-            
-            trending = t
-            upcoming = u
-            nowPlaying = n
-            topRated = tr
-            genres = g
-        } catch {
-            print("Error: \(error)")
-            trending = []
-            upcoming = []
-            nowPlaying = []
-            topRated = []
-            genres = []
-        }
-        
+        // Tạm tắt API để test
         isLoading = false
     }
 }
