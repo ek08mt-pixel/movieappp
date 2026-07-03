0075d96f417ceb22ce32e3ba75e3b1885488b9c4
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-04 05:35:46 +0700

    Update BookingView.swift

--- BookingView.swift
+++ BookingView.swift
@@ -1,25 +1,36 @@
 import SwiftUI
 
 struct BookingView: View {
-    let movie: Movie
-    
+    let cinemas: [Cinema]
+    @Environment(\.dismiss) var dismiss
+
     var body: some View {
-        List(movie.cinemas) { cinema in
-            Button(action: {
-                if let url = URL(string: cinema.bookingURL) {
-                    UIApplication.shared.open(url)
-                }
-            }) {
-                HStack {
-                    Text(cinema.name)
-                        .foregroundColor(.white)
-                    Spacer()
-                    Image(systemName: "chevron.right")
-                        .foregroundColor(.gray)
+        NavigationStack {
+            List(cinemas) { cinema in
+                Button(action: {
+                    if let url = URL(string: cinema.bookingURL) {
+                        UIApplication.shared.open(url)
+                    }
+                }) {
+                    HStack {
+                        Text(cinema.name)
+                            .foregroundColor(.white)
+                            .padding(.vertical, 8)
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
+                    }
                 }
             }
+            .navigationTitle("Chọn Rạp")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Đóng") { dismiss() }
+                        .foregroundColor(.white)
+                }
+            }
+            .background(Color.black)
+            .scrollContentBackground(.hidden)
         }
-        .navigationTitle("Chọn Rạp")
-        .background(Color.black)
     }
 }
