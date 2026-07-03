7a90c213f3b70a56f9839ce054a971ffd0304e7f
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-04 05:52:56 +0700

    Update BookingView.swift

--- BookingView.swift
+++ BookingView.swift
@@ -6,31 +6,48 @@ struct BookingView: View {
 
     var body: some View {
         NavigationStack {
-            List(cinemas) { cinema in
-                Button(action: {
-                    if let url = URL(string: cinema.bookingURL) {
-                        UIApplication.shared.open(url)
-                    }
-                }) {
-                    HStack {
-                        Text(cinema.name)
-                            .foregroundColor(.white)
-                            .padding(.vertical, 8)
-                        Spacer()
-                        Image(systemName: "chevron.right")
+            Group {
+                if cinemas.isEmpty {
+                    VStack(spacing: 15) {
+                        Image(systemName: "ticket")
+                            .font(.system(size: 50))
                             .foregroundColor(.gray)
+                        Text("Chưa có thông tin rạp")
+                            .foregroundColor(.white)
                     }
+                } else {
+                    List(cinemas) { cinema in
+                        Button(action: {
+                            if let url = URL(string: cinema.bookingURL) {
+                                UIApplication.shared.open(url)
+                            }
+                        }) {
+                            HStack {
+                                Text(cinema.name)
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                                    .foregroundColor(.gray)
+                            }
+                            .padding(.vertical, 8)
+                        }
+                        .listRowBackground(Color.gray.opacity(0.1))
+                    }
+                    .scrollContentBackground(.hidden)
                 }
             }
             .navigationTitle("Chọn Rạp")
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button("Đóng") { dismiss() }
-                        .foregroundColor(.white)
+                        .foregroundColor(.blue)
                 }
             }
             .background(Color.black)
-            .scrollContentBackground(.hidden)
+        }
+        .onAppear {
+            print("BookingView đã nhận được \(cinemas.count) rạp.")
         }
     }
 }
