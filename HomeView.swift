6afe34e11ba9f13d74bea0417cdf51473f1dd4c8
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-04 06:36:15 +0700

    Update HomeView.swift

--- HomeView.swift
+++ HomeView.swift
@@ -1,18 +1,40 @@
 import SwiftUI
 
 struct HomeView: View {
-    @StateObject var vm = HomeViewModel()
+    @StateObject private var vm = HomeViewModel()
     
     var body: some View {
-        ZStack {
-            Color.black.ignoresSafeArea()
+        NavigationStack {
             ScrollView {
-                VStack {
-                    Text("Xu hướng").foregroundColor(.white).padding()
-                    // Các nội dung khác
+                VStack(alignment: .leading) {
+                    Text("Xu hướng").font(.title2).bold().foregroundColor(.white).padding()
+                    
+                    ScrollView(.horizontal) {
+                        HStack {
+                            ForEach(vm.trendingMovies) { movie in
+                                NavigationLink(destination: MovieDetailView(movie: movie)) {
+                                    AsyncImage(url: movie.posterURL).frame(width: 140, height: 210).clipShape(RoundedRectangle(cornerRadius: 12))
+                                }
+                            }
+                        }
+                    }
+                    
+                    Text("Đang chiếu rạp").font(.title2).bold().foregroundColor(.white).padding()
+                    
+                    LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())]) {
+                        ForEach(vm.nowPlayingMovies) { movie in
+                            NavigationLink(destination: MovieDetailView(movie: movie)) {
+                                VStack {
+                                    AsyncImage(url: movie.posterURL).frame(height: 200).clipShape(RoundedRectangle(cornerRadius: 12))
+                                    Text(movie.title).foregroundColor(.white).lineLimit(1)
+                                }
+                            }
+                        }
+                    }
                 }
             }
+            .background(Color.black)
+            .task { await vm.loadMovies() }
         }
-        .task { await vm.loadMovies() }
     }
 }
