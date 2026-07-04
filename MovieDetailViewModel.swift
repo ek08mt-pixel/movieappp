d7f62f2c0182302307e06dab4129dd577e25fbb0
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-04 13:50:30 +0700

    Create MovieDetailViewModel.swift

---
+++ MovieDetailViewModel.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+@MainActor
+class MovieDetailViewModel: ObservableObject {
+    @Published var trailerKey: String?
+    @Published var actors: [Actor] = []
+    @Published var similar: [Movie] = []
+    
+    func load(movieId: Int) async {
+        async let t = APIService.shared.trailer(movieId: movieId)
+        async let a = APIService.shared.actors(movieId: movieId)
+        async let s = APIService.shared.similar(movieId: movieId)
+        do {
+            trailerKey = try await t
+            actors = try await a
+            similar = try await s
+        } catch {
+            print("Error: \(error)")
+        }
+    }
+}
