30c9fa31b02cc9f43534b079091209a8e4ce263c
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-04 17:24:45 +0700

    Update APIService.swift

--- APIService.swift
+++ APIService.swift
@@ -14,7 +14,15 @@ class APIService {
         return d
     }()
     
-    func trending() async throws -> [Movie] {
+    func trending24h() async throws -> [Movie] {
+        let urlString = "\(baseURL)/trending/movie/day?api_key=\(apiKey)&language=\(language)"
+        guard let url = URL(string: urlString) else { return [] }
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let response = try decoder.decode(MovieResponse.self, from: data)
+        return response.results
+    }
+    
+    func trendingWeek() async throws -> [Movie] {
         let urlString = "\(baseURL)/trending/movie/week?api_key=\(apiKey)&language=\(language)"
         guard let url = URL(string: urlString) else { return [] }
         let (data, _) = try await URLSession.shared.data(from: url)
@@ -54,8 +62,24 @@ class APIService {
         return response.results
     }
     
-    func asianMovies() async throws -> [Movie] {
-        let urlString = "\(baseURL)/discover/movie?api_key=\(apiKey)&with_original_language=ko|ja|zh&sort_by=popularity.desc&language=\(language)"
+    func koreanMovies() async throws -> [Movie] {
+        let urlString = "\(baseURL)/discover/movie?api_key=\(apiKey)&with_original_language=ko&sort_by=popularity.desc&language=\(language)"
+        guard let url = URL(string: urlString) else { return [] }
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let response = try decoder.decode(MovieResponse.self, from: data)
+        return response.results
+    }
+    
+    func japaneseMovies() async throws -> [Movie] {
+        let urlString = "\(baseURL)/discover/movie?api_key=\(apiKey)&with_original_language=ja&sort_by=popularity.desc&language=\(language)"
+        guard let url = URL(string: urlString) else { return [] }
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let response = try decoder.decode(MovieResponse.self, from: data)
+        return response.results
+    }
+    
+    func vietnameseMovies() async throws -> [Movie] {
+        let urlString = "\(baseURL)/discover/movie?api_key=\(apiKey)&with_original_language=vi&sort_by=popularity.desc&language=\(language)"
         guard let url = URL(string: urlString) else { return [] }
         let (data, _) = try await URLSession.shared.data(from: url)
         let response = try decoder.decode(MovieResponse.self, from: data)
@@ -95,12 +119,30 @@ class APIService {
         return response.results
     }
     
+    func discoverMovies(year: Int? = nil, genreId: Int? = nil, minRating: Double? = nil) async throws -> [Movie] {
+        var urlString = "\(baseURL)/discover/movie?api_key=\(apiKey)&sort_by=popularity.desc&language=\(language)"
+        if let year = year { urlString += "&primary_release_year=\(year)" }
+        if let genreId = genreId { urlString += "&with_genres=\(genreId)" }
+        if let minRating = minRating { urlString += "&vote_average.gte=\(minRating)" }
+        guard let url = URL(string: urlString) else { return [] }
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let response = try decoder.decode(MovieResponse.self, from: data)
+        return response.results
+    }
+    
     func similar(movieId: Int) async throws -> [Movie] {
         let urlString = "\(baseURL)/movie/\(movieId)/similar?api_key=\(apiKey)&language=\(language)"
         guard let url = URL(string: urlString) else { return [] }
         let (data, _) = try await URLSession.shared.data(from: url)
         let response = try decoder.decode(MovieResponse.self, from: data)
         return response.results
+    }
+    
+    func movieDetail(movieId: Int) async throws -> MovieDetail? {
+        let urlString = "\(baseURL)/movie/\(movieId)?api_key=\(apiKey)&language=\(language)&append_to_response=credits"
+        guard let url = URL(string: urlString) else { return nil }
+        let (data, _) = try await URLSession.shared.data(from: url)
+        return try? decoder.decode(MovieDetail.self, from: data)
     }
     
     func trailer(movieId: Int) async throws -> String? {
