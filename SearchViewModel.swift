8b217a35a1bfd4b393f461df70d8200d6817c16d
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-04 17:28:38 +0700

    Update SearchViewModel.swift

--- SearchViewModel.swift
+++ SearchViewModel.swift
@@ -4,7 +4,16 @@ import Foundation
 class SearchViewModel: ObservableObject {
     @Published var query = ""
     @Published var results: [Movie] = []
+    @Published var trending: [Movie] = []
     private var task: Task<Void, Never>?
+    
+    func loadTrending() async {
+        do {
+            trending = try await APIService.shared.trending24h()
+        } catch {
+            trending = []
+        }
+    }
     
     func search() async {
         task?.cancel()
