f32c7468d54864d9326e44c452e89825b1e7703a
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-06 23:08:36 +0700

    Update AppEntry.swift

--- AppEntry.swift
+++ AppEntry.swift
@@ -9,33 +9,19 @@ class AppState: ObservableObject {
     @Published var selectedAvatar = "person.circle.fill"
     @Published var avatarImageData: Data?
     
-    private let cloudStore = NSUbiquitousKeyValueStore.default
-    
-    init() {
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(cloudChanged),
-            name: NSUbiquitousKeyValueStore.didChangeExternallyNotification,
-            object: cloudStore
-        )
-        load()
-    }
-    
-    @objc func cloudChanged(_ notification: Notification) {
-        DispatchQueue.main.async { self.load() }
-    }
+    init() { load() }
     
     func register(email: String, password: String) {
         self.email = email
         self.isLoggedIn = true
-        cloudStore.set(email, forKey: "registeredEmail")
-        cloudStore.set(password, forKey: "registeredPassword")
+        UserDefaults.standard.set(email, forKey: "registeredEmail")
+        UserDefaults.standard.set(password, forKey: "registeredPassword")
         save()
     }
     
     func login(email: String, password: String) {
-        let savedEmail = cloudStore.string(forKey: "registeredEmail") ?? ""
-        let savedPassword = cloudStore.string(forKey: "registeredPassword") ?? ""
+        let savedEmail = UserDefaults.standard.string(forKey: "registeredEmail") ?? ""
+        let savedPassword = UserDefaults.standard.string(forKey: "registeredPassword") ?? ""
         if email == savedEmail && password == savedPassword {
             self.email = email
             self.isLoggedIn = true
@@ -55,25 +41,24 @@ class AppState: ObservableObject {
     }
     
     func save() {
-        cloudStore.set(isLoggedIn, forKey: "isLoggedIn")
-        cloudStore.set(email, forKey: "email")
-        cloudStore.set(nickname, forKey: "nickname")
-        cloudStore.set(selectedAvatar, forKey: "avatar")
-        cloudStore.set(avatarImageData, forKey: "avatarImage")
-        if let fav = try? JSONEncoder().encode(favorites) { cloudStore.set(fav, forKey: "favorites") }
-        if let hist = try? JSONEncoder().encode(watchHistory) { cloudStore.set(hist, forKey: "history") }
-        cloudStore.synchronize()
+        UserDefaults.standard.set(isLoggedIn, forKey: "isLoggedIn")
+        UserDefaults.standard.set(email, forKey: "email")
+        UserDefaults.standard.set(nickname, forKey: "nickname")
+        UserDefaults.standard.set(selectedAvatar, forKey: "avatar")
+        UserDefaults.standard.set(avatarImageData, forKey: "avatarImage")
+        if let fav = try? JSONEncoder().encode(favorites) { UserDefaults.standard.set(fav, forKey: "favorites") }
+        if let hist = try? JSONEncoder().encode(watchHistory) { UserDefaults.standard.set(hist, forKey: "history") }
     }
     
     func load() {
-        isLoggedIn = cloudStore.bool(forKey: "isLoggedIn")
-        email = cloudStore.string(forKey: "email") ?? ""
-        nickname = cloudStore.string(forKey: "nickname") ?? ""
-        selectedAvatar = cloudStore.string(forKey: "avatar") ?? "person.circle.fill"
-        avatarImageData = cloudStore.data(forKey: "avatarImage")
-        if let fav = cloudStore.data(forKey: "favorites"),
+        isLoggedIn = UserDefaults.standard.bool(forKey: "isLoggedIn")
+        email = UserDefaults.standard.string(forKey: "email") ?? ""
+        nickname = UserDefaults.standard.string(forKey: "nickname") ?? ""
+        selectedAvatar = UserDefaults.standard.string(forKey: "avatar") ?? "person.circle.fill"
+        avatarImageData = UserDefaults.standard.data(forKey: "avatarImage")
+        if let fav = UserDefaults.standard.data(forKey: "favorites"),
            let f = try? JSONDecoder().decode([Movie].self, from: fav) { favorites = f }
-        if let hist = cloudStore.data(forKey: "history"),
+        if let hist = UserDefaults.standard.data(forKey: "history"),
            let h = try? JSONDecoder().decode([Movie].self, from: hist) { watchHistory = h }
     }
 }
