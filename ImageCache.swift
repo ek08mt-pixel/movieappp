7c2c6eca489a8f32601470687d96599d57900d85
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-06 19:40:02 +0700

    Update ImageCache.swift

--- ImageCache.swift
+++ ImageCache.swift
@@ -4,6 +4,11 @@ class ImageCache {
     static let shared = ImageCache()
     private var cache = NSCache<NSString, UIImage>()
     
+    init() {
+        cache.countLimit = 300
+        cache.totalCostLimit = 100 * 1024 * 1024
+    }
+    
     func get(for url: URL) -> UIImage? {
         return cache.object(forKey: url.absoluteString as NSString)
     }
@@ -16,6 +21,7 @@ class ImageCache {
 struct CachedAsyncImage: View {
     let url: URL?
     @State private var image: UIImage?
+    @State private var isLoading = false
     
     var body: some View {
         Group {
@@ -23,22 +29,36 @@ struct CachedAsyncImage: View {
                 Image(uiImage: image)
                     .resizable().aspectRatio(contentMode: .fill)
             } else {
-                Rectangle().fill(Color.gray.opacity(0.08))
+                Rectangle()
+                    .fill(LinearGradient(colors: [Color(white: 0.15), Color(white: 0.08)], startPoint: .top, endPoint: .bottom))
                     .task {
-                        guard let url = url else { return }
+                        guard let url = url, !isLoading else { return }
+                        isLoading = true
                         if let cached = ImageCache.shared.get(for: url) {
-                            image = cached
+                            await MainActor.run { image = cached }
                         } else {
                             do {
-                                let (data, _) = try await URLSession.shared.data(from: url)
+                                let request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 15)
+                                let (data, _) = try await URLSession.shared.data(for: request)
                                 if let img = UIImage(data: data) {
-                                    ImageCache.shared.set(img, for: url)
-                                    image = img
+                                    let resized = img.resized(to: CGSize(width: 400, height: 600))
+                                    ImageCache.shared.set(resized, for: url)
+                                    await MainActor.run { image = resized }
                                 }
                             } catch {}
                         }
+                        isLoading = false
                     }
             }
+        }
+    }
+}
+
+extension UIImage {
+    func resized(to size: CGSize) -> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: size)
+        return renderer.image { _ in
+            self.draw(in: CGRect(origin: .zero, size: size))
         }
     }
 }
