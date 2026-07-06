423b61cc15bdebd9becdbd8442607f146702ed5f
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-07 01:22:47 +0700

    Update APIService.swift

--- APIService.swift
+++ APIService.swift
@@ -129,9 +129,22 @@ class APIService {
         let urlString = "\(baseURL)/search/tv?api_key=\(apiKey)&language=en-US&query=\(encoded)&page=\(page)"
         guard let url = URL(string: urlString) else { return [] }
         let (data, _) = try await URLSession.shared.data(from: url)
-        let response = try decoder.decode(MovieResponse.self, from: data)
-        return response.results.map { movie in
-            Movie(id: movie.id, title: movie.title, overview: movie.overview, posterPath: movie.posterPath, backdropPath: movie.backdropPath, voteAverage: movie.voteAverage, releaseDate: movie.releaseDate, genreIds: movie.genreIds, originalTitle: movie.originalTitle, popularity: movie.popularity, voteCount: movie.voteCount, adult: movie.adult, originalLanguage: movie.originalLanguage, mediaType: "tv")
+        struct TVSearchResponse: Codable { let results: [TVResult] }
+        struct TVResult: Codable {
+            let id: Int; let name: String?; let overview: String
+            let poster_path: String?; let backdrop_path: String?
+            let vote_average: Double; let first_air_date: String?
+            let genre_ids: [Int]?; let popularity: Double?
+            let vote_count: Int?; let original_language: String?
+        }
+        let response = try JSONDecoder().decode(TVSearchResponse.self, from: data)
+        return response.results.map { tv in
+            Movie(id: tv.id, title: tv.name ?? "Unknown", overview: tv.overview,
+                  posterPath: tv.poster_path, backdropPath: tv.backdrop_path,
+                  voteAverage: tv.vote_average, releaseDate: tv.first_air_date,
+                  genreIds: tv.genre_ids, originalTitle: tv.name,
+                  popularity: tv.popularity, voteCount: tv.vote_count,
+                  adult: false, originalLanguage: tv.original_language, mediaType: "tv")
         }
     }
     
@@ -292,4 +305,4 @@ extension Movie {
     func withPlaceholder() -> Movie {
         return Movie(id: id, title: title, overview: overview, posterPath: posterPath ?? "/placeholder.jpg", backdropPath: backdropPath, voteAverage: voteAverage, releaseDate: releaseDate, genreIds: genreIds, originalTitle: originalTitle, popularity: popularity, voteCount: voteCount, adult: adult, originalLanguage: originalLanguage, mediaType: mediaType)
     }
-} 
+}
