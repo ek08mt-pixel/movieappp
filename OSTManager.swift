545a0378df6c54c232b1d9b1e053088eb678916f
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-08 02:11:25 +0700

    Delete OSTManager.swift

--- OSTManager.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-class OSTManager: ObservableObject {
-    static let shared = OSTManager()
-    @Published var isPlaying = false
-    @Published var currentTrack: String = ""
-    @Published var currentMovie: String = ""
-}
