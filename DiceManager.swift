34dab229492b14257027572add2bdb25be284426
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-08 12:15:24 +0700

    Delete DiceManager.swift

--- DiceManager.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-class DiceManager: ObservableObject {
-    static let shared = DiceManager()
-    @Published var showDice = false
-    @Published var selectedMovie: Movie?
-    @Published var isRolling = false
-}
