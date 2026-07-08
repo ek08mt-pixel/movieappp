198f6b6c03208ca2776237549188c4aab6b017dd
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-08 14:50:58 +0700

    Update OSTManager.swift

--- OSTManager.swift
+++ OSTManager.swift
@@ -7,7 +7,15 @@ class OSTManager: ObservableObject {
     @Published var currentTrack: String = ""
     @Published var currentMovie: String = ""
     @Published var currentPoster: String? = nil
+    @Published var showOSTView = false
+    @Published var miniMode: MiniMode = .normal
     @Published var audioPlayer: AVPlayer?
+    
+    enum MiniMode: Int, CaseIterable {
+        case normal = 0
+        case bubble = 1
+        case dot = 2
+    }
     
     var togglePlayback: (() -> Void)?
     var stopPlayback: (() -> Void)?
