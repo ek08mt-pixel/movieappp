0066ffcf6fb89ab84322008e6d2285bea88ba449
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-10 01:13:10 +0700

    Update WatchTogetherService.swift

--- WatchTogetherService.swift
+++ WatchTogetherService.swift
@@ -6,7 +6,7 @@ class WatchTogetherService: ObservableObject {
     
     private let baseURL = "https://emmew-d71a8-default-rtdb.firebaseio.com"
     private var roomCode: String = ""
-    userId: String = ""
+    var userId: String = ""
     private var userName: String = ""
     private var timer: Timer?
     
