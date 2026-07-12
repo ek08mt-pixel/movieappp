c8943119c18eca679fd15d4884da541a2eaa285a
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-12 13:29:14 +0700

    Update WatchTogetherRoomView.swift

--- WatchTogetherRoomView.swift
+++ WatchTogetherRoomView.swift
@@ -127,7 +127,7 @@ struct WatchTogetherRoomView: View {
             } else {
                 VStack(spacing: 0) {
                     WatchPlayerVC(player: player, pipController: $pipController)
-                        .frame(height: geo.size.width * 9 / 16 + 50)
+                        .frame(height: geo.size.width * 9 / 16 - 50)
                         .overlay(
                             videoControlsOverlay
                                 .allowsHitTesting(showControls)
