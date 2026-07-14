4a53421715fd195cd63dcf04cd7e759a8a24fbcc
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-14 19:21:00 +0700

    Create DownloadedPlayerView.swift

---
+++ DownloadedPlayerView.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+import AVKit
+
+struct DownloadedPlayerView: View {
+    let url: URL
+    let title: String
+    @Environment(\.dismiss) var dismiss
+    @State private var player: AVPlayer?
+    
+    var body: some View {
+        ZStack {
+            Color.black.ignoresSafeArea()
+            if let player = player {
+                VideoPlayer(player: player)
+                    .ignoresSafeArea()
+                    .onAppear { player.play() }
+                    .onDisappear { player.pause() }
+            }
+            
+            VStack {
+                HStack {
+                    Button { dismiss() } label: {
+                        Image(systemName: "chevron.left")
+                            .font(.system(size: 16, weight: .semibold))
+                            .foregroundColor(.white)
+                            .padding(10)
+                            .background(Circle().fill(.ultraThinMaterial.opacity(0.4)))
+                    }
+                    Spacer()
+                    Text(title).font(.caption).foregroundColor(.white).lineLimit(1)
+                    Spacer()
+                    Circle().fill(.clear).frame(width: 36)
+                }
+                .padding(.horizontal, 16).padding(.top, 50)
+                Spacer()
+            }
+        }
+        .onAppear { player = AVPlayer(url: url) }
+    }
+}
