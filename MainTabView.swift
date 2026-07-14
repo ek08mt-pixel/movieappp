ad47ce52f4a3f809cb1656ed355388f72f953dd6
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-14 15:44:13 +0700

    Update MainTabView.swift

--- MainTabView.swift
+++ MainTabView.swift
@@ -77,7 +77,7 @@ struct MainTabView: View {
                         Image(systemName: "magnifyingglass")
                             .font(.system(size: 22, weight: .medium))
                             .foregroundColor(.white.opacity(0.7))
-                            .padding(.vertical, 18).padding(.horizontal, 20)
+                            .padding(.vertical, 12).padding(.horizontal, 20)
                             .background(
                                 Capsule()
                                     .fill(.ultraThinMaterial.opacity(0.5))
