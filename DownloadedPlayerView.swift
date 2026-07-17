2e460d0eda383a8dd79c7c052cdbcba1cf48721c
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-18 00:23:55 +0700

    Update DownloadedPlayerView.swift

--- DownloadedPlayerView.swift
+++ DownloadedPlayerView.swift
@@ -3,6 +3,8 @@ import AVKit
 
 class LocalAssetLoader: NSObject, AVAssetResourceLoaderDelegate {
     let folderURL: URL
+    var masterContent: String = ""
+    var subContent: String = ""
     
     init(folderURL: URL) {
         self.folderURL = folderURL
@@ -12,8 +14,54 @@ class LocalAssetLoader: NSObject, AVAssetResourceLoaderDelegate {
         guard let url = loadingRequest.request.url else { return false }
         
         let fileName = url.lastPathComponent
-        let fileURL = folderURL.appendingPathComponent(fileName)
         
+        if fileName == "master.m3u8" {
+            // Đọc master.m3u8 từ folder
+            let masterURL = folderURL.appendingPathComponent("master.m3u8")
+            if let content = try? String(contentsOf: masterURL, encoding: .utf8) {
+                masterContent = content
+                var lines = content.components(separatedBy: .newlines)
+                var fixedLines: [String] = []
+                
+                for line in lines {
+                    let trimmed = line.trimmingCharacters(in: .whitespaces)
+                    if !trimmed.hasPrefix("#") && !trimmed.isEmpty && trimmed.hasSuffix(".m3u8") {
+                        fixedLines.append("local-hls://playlist/sub.m3u8")
+                    } else {
+                        fixedLines.append(line)
+                    }
+                }
+                
+                let data = fixedLines.joined(separator: "\n").data(using: .utf8)!
+                loadingRequest.dataRequest?.respond(with: data)
+                loadingRequest.finishLoading()
+                return true
+            }
+        } else if fileName == "sub.m3u8" {
+            let subURL = folderURL.appendingPathComponent("sub.m3u8")
+            if let content = try? String(contentsOf: subURL, encoding: .utf8) {
+                var lines = content.components(separatedBy: .newlines)
+                var fixedLines: [String] = []
+                
+                for line in lines {
+                    let trimmed = line.trimmingCharacters(in: .whitespaces)
+                    if !trimmed.hasPrefix("#") && !trimmed.isEmpty && trimmed.hasSuffix(".ts") {
+                        let tsURL = folderURL.appendingPathComponent(trimmed)
+                        fixedLines.append(tsURL.absoluteString)
+                    } else {
+                        fixedLines.append(line)
+                    }
+                }
+                
+                let data = fixedLines.joined(separator: "\n").data(using: .utf8)!
+                loadingRequest.dataRequest?.respond(with: data)
+                loadingRequest.finishLoading()
+                return true
+            }
+        }
+        
+        // Fallback: thử đọc file trực tiếp từ folder
+        let fileURL = folderURL.appendingPathComponent(fileName)
         if let data = try? Data(contentsOf: fileURL) {
             loadingRequest.dataRequest?.respond(with: data)
             loadingRequest.finishLoading()
@@ -59,13 +107,10 @@ struct DownloadedPlayerView: View {
     
     private func playLocalVideo() {
         let folderURL = url.deletingLastPathComponent()
-        
-        // Tạo custom scheme URL
         let customURL = URL(string: "local-hls://playlist/master.m3u8")!
         let asset = AVURLAsset(url: customURL)
         let loader = LocalAssetLoader(folderURL: folderURL)
         asset.resourceLoader.setDelegate(loader, queue: .main)
-        
         player = AVPlayer(playerItem: AVPlayerItem(asset: asset))
     }
 }
