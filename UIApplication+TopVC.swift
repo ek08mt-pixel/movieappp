76b6cbbc7a3c348361ff664ed5e45fb8cf424190
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-18 17:57:01 +0700

    Create UIApplication+TopVC.swift

---
+++ UIApplication+TopVC.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+extension UIApplication {
+    static func topViewController(
+        base: UIViewController? = UIApplication.shared.connectedScenes
+            .compactMap { $0 as? UIWindowScene }
+            .flatMap { $0.windows }
+            .first(where: { $0.isKeyWindow })?.rootViewController
+    ) -> UIViewController? {
+        if let nav = base as? UINavigationController {
+            return topViewController(base: nav.visibleViewController)
+        }
+        if let tab = base as? UITabBarController,
+           let selected = tab.selectedViewController {
+            return topViewController(base: selected)
+        }
+        if let presented = base?.presentedViewController {
+            return topViewController(base: presented)
+        }
+        return base
+    }
+}
