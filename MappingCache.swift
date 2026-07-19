6b876d502fb9614fbcd7e00dd89838a8df18a97d
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-19 15:47:39 +0700

    Update MappingCache.swift

--- MappingCache.swift
+++ MappingCache.swift
@@ -33,6 +33,7 @@ final class MappingCache {
         "111110_1": "dao-hai-tac-live-action-phan-1",
         "111110_2": "dao-hai-tac-live-action-phan-2",
         "222624_1": "gintama-thay-ginpachi-o-lop-3z",
+        "46261_1": "hoi-phap-su",
     ]
     
     static func getAnimeSlug(tmdbID: Int) -> String? { animeSlugs[tmdbID] }
