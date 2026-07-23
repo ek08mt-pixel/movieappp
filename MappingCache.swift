c03361af5a089a8a9e52abe04eaebac93e863e6c
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-23 21:38:30 +0700

    Update MappingCache.swift

--- MappingCache.swift
+++ MappingCache.swift
@@ -71,7 +71,7 @@ final class MappingCache {
 "1408_6": "bac-si-house-phan-6",
 "1408_7": "bac-si-house-phan-7",
 "1408_8": "bac-si-house-phan-8",
-"1639_3": "giai-cuu-the-gioi-phan-3"
+"1639_3": "giai-cuu-the-gioi-phan-3",
 "1639_4": "giai-cuu-the-gioi-phan-4","1110034": "quai-vat-bien", "128098_1": "phong-van-ma-ca-rong-phan-1",
 "128098_2": "phong-van-ma-ca-rong-phan-2","1981_1": "phep-thuat-phan-1",
  "1981_2": "phep-thuat-phan-2", 
@@ -97,7 +97,7 @@ final class MappingCache {
 "1973_6": "24-gio-chong-khung-bo-phan-6",
 "1973_7": "24-gio-chong-khung-bo-phan-7",
 "1973_8": "24-gio-chong-khung-bo-phan-8",
-"1973_9": "24-gio-chong-khung-bo-phan-9"
+"1973_9": "24-gio-chong-khung-bo-phan-9",
     ]
     
     static func getAnimeSlug(tmdbID: Int) -> String? { animeSlugs[tmdbID] }
