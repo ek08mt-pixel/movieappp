150061e67083d5e278df2926d251e77d5949e529
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-24 21:31:46 +0700

    Update MovieDetailView.swift

--- MovieDetailView.swift
+++ MovieDetailView.swift
@@ -74,6 +74,7 @@ struct MovieDetailView: View {
                                     .font(.system(size: 22, weight: .bold))
                                     .foregroundColor(.white)
                                     Text("Slug: \(MappingCache.getDirectSlug(tmdbID: movie.id, season: 1) ?? "nil") | Ep: \(vm.sourceEpisodes.count)").font(.system(size: 8)).foregroundColor(.gray)
+                                Text("ID: \(movie.id) Season: \(vm.seasons.first?.seasonNumber ?? 0)").font(.system(size: 8)).foregroundColor(.gray)
                                 HStack(spacing: 6) {
                                     Text(releaseDateText).foregroundColor(.gray).font(.caption)
                                     Text("•").foregroundColor(.gray)
