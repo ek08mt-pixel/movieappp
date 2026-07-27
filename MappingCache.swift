1ae2f21580cab59c95d82f2ed944426f1150b7c2
ek08mt-pixel <ek08.mt@gmail.com>
2026-07-28 00:53:30 +0700

    Update MappingCache.swift

--- MappingCache.swift
+++ MappingCache.swift
@@ -117,7 +117,7 @@ final class MappingCache {
     func getHardcodedSlug(tmdbID: Int, season: Int) -> String? { hardcodedSlugs["\(tmdbID)_\(season)"] }
     func dict(for key: String) -> [String: String] { defaults.dictionary(forKey: key) as? [String: String] ?? [:] }
     func save(_ dict: [String: String], for key: String) { defaults.set(dict, forKey: key) }
-    func clearAll() { defaults.removeObject(forKey: nguonCKey); defaults.removeObject(forKey: vsmovKey); defaults.removeObject(forKey: stravoKey); defaults.removeObject(forKey: phimapiKey); defaults.removeObject(forKey: sofaflixKey) }
+    func clearAll() { defaults.removeObject(forKey: nguonCKey); defaults.removeObject(forKey: vsmovKey); defaults.removeObject(forKey: phimapiKey) }
 }
 
 // MARK: - Error
