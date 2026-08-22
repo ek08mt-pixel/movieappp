c18bf12521d1a69200d57925d784987acb966ca4
ek08mt-pixel <ek08.mt@gmail.com>
2026-08-23 03:35:38 +0700

    Update PhimFunExtractor.swift

--- PhimFunExtractor.swift
+++ PhimFunExtractor.swift
@@ -2,31 +2,40 @@ import WebKit
 
 class PhimFunExtractor: NSObject, WKNavigationDelegate, WKScriptMessageHandler {
     static let shared = PhimFunExtractor()
-    private var webView: WKWebView!
+    private var webView: WKWebView?
     private var completion: ((String?) -> Void)?
     
     func extractStreamURL(embedURL: String, completion: @escaping (String?) -> Void) {
-        self.completion = completion
-        
-        let config = WKWebViewConfiguration()
-        config.userContentController.add(self, name: "phimFunExtractor")
-        
-        let script = """
-        setTimeout(function() {
-            var iframe = document.getElementById('embedIframe') || document.getElementById('iframeStream');
-            if (iframe && iframe.src && iframe.src !== '') {
-                window.webkit.messageHandlers.phimFunExtractor.postMessage(iframe.src);
-            } else {
-                window.webkit.messageHandlers.phimFunExtractor.postMessage('null');
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            self.completion = completion
+            
+            guard let url = URL(string: embedURL) else {
+                completion(nil)
+                return
             }
-        }, 8000);
-        """
-        let userScript = WKUserScript(source: script, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
-        config.userContentController.addUserScript(userScript)
-        
-        webView = WKWebView(frame: .zero, configuration: config)
-        webView.navigationDelegate = self
-        webView.load(URLRequest(url: URL(string: embedURL)!))
+            
+            let config = WKWebViewConfiguration()
+            config.userContentController.add(self, name: "phimFunExtractor")
+            
+            let script = """
+            setTimeout(function() {
+                var iframe = document.getElementById('embedIframe') || document.getElementById('iframeStream');
+                if (iframe && iframe.src && iframe.src !== '') {
+                    window.webkit.messageHandlers.phimFunExtractor.postMessage(iframe.src);
+                } else {
+                    window.webkit.messageHandlers.phimFunExtractor.postMessage('null');
+                }
+            }, 8000);
+            """
+            let userScript = WKUserScript(source: script, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+            config.userContentController.addUserScript(userScript)
+            
+            let webView = WKWebView(frame: .zero, configuration: config)
+            webView.navigationDelegate = self
+            self.webView = webView
+            webView.load(URLRequest(url: url))
+        }
     }
     
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
@@ -37,8 +46,9 @@ class PhimFunExtractor: NSObject, WKNavigationDelegate, WKScriptMessageHandler {
             } else {
                 completion?(nil)
             }
-            webView.stopLoading()
+            webView?.stopLoading()
             webView = nil
+            completion = nil
         }
     }
 }
