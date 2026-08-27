f8a752b58b1dd38571ef61fddd3ae445db9c4a7f
ek08mt-pixel <ek08.mt@gmail.com>
2026-08-27 13:51:23 +0700

    Update HLSResourceLoader.swift

--- HLSResourceLoader.swift
+++ HLSResourceLoader.swift
@@ -1,65 +1,69 @@
 import Foundation
 import AVFoundation
 
-final class HLSResourceLoader: NSObject, AVAssetResourceLoaderDelegate {
-    static let shared = HLSResourceLoader()
-    
+final class HLSResourceLoaderDelegate: NSObject, AVAssetResourceLoaderDelegate {
     private let session: URLSession
+    private var pendingRequests: [URLRequest: URLSessionDataTask] = [:]
     
-    override private init() {
+    override init() {
         let config = URLSessionConfiguration.default
         config.httpAdditionalHeaders = [
             "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1",
             "Referer": "https://film4k.net/",
-            "Origin": "https://film4k.net",
-            "Accept": "*/*"
+            "Accept": "application/vnd.apple.mpegurl,*/*"
         ]
         session = URLSession(configuration: config)
         super.init()
     }
     
-    func resourceLoader(
-        _ resourceLoader: AVAssetResourceLoader,
-        shouldWaitForLoadingOfRequestedResource loadingRequest: AVAssetResourceLoadingRequest
-    ) -> Bool {
+    func resourceLoader(_ resourceLoader: AVAssetResourceLoader,
+                        shouldWaitForLoadingOfRequestedResource loadingRequest: AVAssetResourceLoadingRequest) -> Bool {
+        guard let url = loadingRequest.request.url else { return false }
         
-        guard let url = loadingRequest.request.url else {
-            loadingRequest.finishLoading()
-            return false
-        }
+        // Chuyển scheme proxyhls:// về https://
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+        components.scheme = "https"
+        let originalURL = components.url!
         
-        // Chuyển scheme custom-hls:// sang https://
-        var realURLString = url.absoluteString
-        if realURLString.hasPrefix("custom-hls://") {
-            realURLString = String(realURLString.dropFirst("custom-hls://".count))
-        }
-        
-        guard let realURL = URL(string: realURLString) else {
-            loadingRequest.finishLoading()
-            return false
-        }
-        
-        var request = URLRequest(url: realURL)
+        var request = URLRequest(url: originalURL)
+        request.timeoutInterval = 15
         request.setValue("Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1", forHTTPHeaderField: "User-Agent")
         request.setValue("https://film4k.net/", forHTTPHeaderField: "Referer")
-        request.setValue("https://film4k.net", forHTTPHeaderField: "Origin")
-        request.setValue("*/*", forHTTPHeaderField: "Accept")
+        request.setValue("application/vnd.apple.mpegurl,*/*", forHTTPHeaderField: "Accept")
         
-        let task = session.dataTask(with: request) { data, response, error in
+        let task = session.dataTask(with: request) { [weak self] data, response, error in
+            guard let self = self else { return }
+            
             if let error = error {
                 loadingRequest.finishLoading(with: error)
                 return
             }
-            if let response = response {
-                loadingRequest.response = response
+            
+            guard let data = data, let httpResponse = response as? HTTPURLResponse else {
+                loadingRequest.finishLoading(with: NSError(domain: "HLS", code: -1))
+                return
             }
-            if let data = data {
-                loadingRequest.dataRequest?.respond(with: data)
-            }
+            
+            loadingRequest.contentInformationRequest?.contentType = httpResponse.mimeType
+            loadingRequest.contentInformationRequest?.contentLength = Int64(data.count)
+            loadingRequest.contentInformationRequest?.isByteRangeAccessSupported = true
+            
+            loadingRequest.dataRequest?.respond(with: data)
             loadingRequest.finishLoading()
+            
+            self.pendingRequests.removeValue(forKey: request)
         }
-        task.resume()
         
+        pendingRequests[request] = task
+        task.resume()
         return true
+    }
+    
+    func resourceLoader(_ resourceLoader: AVAssetResourceLoader,
+                        didCancel loadingRequest: AVAssetResourceLoadingRequest) {
+        if let url = loadingRequest.request.url {
+            pendingRequests[url]?.cancel()
+            pendingRequests.removeValue(forKey: url)
+        }
     }
 }
